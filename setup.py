3863b940576a60c8ed38a715c2383127fee912fb
Ben Winston <ben@benwinston.us>
2023-02-28 22:03:46 -0500

    i'm bad at pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(name="roku-tui",
-        version="0.1.1",
+        version="0.1.2",
         description="A TUI remote for Roku",
         long_description=long_description,
         long_description_content_type="text/markdown",
