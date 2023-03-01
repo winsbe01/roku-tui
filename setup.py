d32d883653ec12c1d4f716373c267fb8becafeb8
Ben Winston <ben@benwinston.us>
2023-02-28 21:03:05 -0500

    version bump 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(name="roku-tui",
-        version="0.1.0",
+        version="0.1.1",
         description="A TUI remote for Roku",
         long_description=long_description,
         long_description_content_type="text/markdown",
